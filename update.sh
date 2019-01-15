15757d22fa19082060203471c2d5b6afd9721164
cheio <achillescheio@gmail.com>
2019-01-15 15:49:54 +0800

    questionare

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+git add .
+if [ $# -lt  1 ]; then
+    echo "$0 <commit message>"
+    exit 1
+fi
+
+msg="$1"
+git commit -m "$msg"
+if [ $? -ne 0 ]; then
+    echo "Commit failed"
+    exit 1
+fi
+
+git push
+if [ $? -ne 0 ]; then
+    echo "Push failed"
+fi
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+git commit -m "$msg"
+
+# Push source and build repos.
+
+git push
+# Come Back up to the Project Root
+cd ..
+
